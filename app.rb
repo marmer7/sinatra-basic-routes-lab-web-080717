5d21715fcbd8306ac3f1280cfc6a6808da13c2f1
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-28 14:14:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Marlon"
+  end
+
+  get '/hometown' do
+    "My hometown is LA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is sturgill"
+  end
 end
